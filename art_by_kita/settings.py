18513aaba3e7447675b110f12eb3066b7ca942e8
Mark Butler <markbutlermb1991@gmail.com>
2025-02-27 20:03:21 +0000

    bug fix on Brevo

--- art_by_kita/settings.py
+++ art_by_kita/settings.py
@@ -18,7 +18,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-BREVO_API_KEY = env('BREVO_API_KEY')
+BREVO_API_KEY = env('BREVO_API_KEY', default="dummy_value")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
