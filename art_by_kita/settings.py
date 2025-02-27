fdc51463f6a6a025d64d7fcf523920546f384a09
Mark Butler <markbutlermb1991@gmail.com>
2025-02-27 20:29:03 +0000

    updated settings for netlify

--- art_by_kita/settings.py
+++ art_by_kita/settings.py
@@ -24,13 +24,14 @@ BREVO_API_KEY = env('BREVO_API_KEY', default="dummy_value")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'art-by-kita-ci-af8ba42cd883.herokuapp.com', 'https://artbykita.netlify.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://art-by-kita-81626722ece0.herokuapp.com/', 'https://artbykita.netlify.app']
 
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     'https://artbykita.netlify.app',
+    'https://art-by-kita-81626722ece0.herokuapp.com',
 ]
 
 # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
@@ -52,7 +53,7 @@ CORS_ALLOW_HEADERS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",  # ✅ Add Vite frontend as a trusted origin
-    "https://art-by-kita-ci-af8ba42cd883.herokuapp.com",
+    "https://art-by-kita-81626722ece0.herokuapp.com/",
     'https://artbykita.netlify.app',
 ]
 
