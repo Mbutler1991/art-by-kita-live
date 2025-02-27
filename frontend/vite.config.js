8e1b233608420beca641e0d7801ca529f29538bc
Mark Butler <markbutlermb1991@gmail.com>
2025-02-27 20:50:45 +0000

    node modules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://art-by-kita-backend.herokuapp.com", // Update to your Heroku backend
+        target: "https://art-by-kita-81626722ece0.herokuapp.com", // Update to your Heroku backend
         changeOrigin: true,
         secure: false,
       },
