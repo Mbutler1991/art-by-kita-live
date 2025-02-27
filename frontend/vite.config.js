f6cc39ea152d24e8b285cdfb3b8ac77c4596ea6b
Mark Butler <markbutlermb1991@gmail.com>
2025-02-27 19:50:54 +0000

    set up for heroku

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Ensure it outputs to 'dist'
+    emptyOutDir: true, // Clears old files before building
+  },
+  server: {
+    port: 3000, // Ensure this is your dev server port
+  },
+});
